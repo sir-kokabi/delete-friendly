6670fed73e8826c396a34aa799faf9edf9da500f
ayub <sir.kokabi@gmail.com>
2023-01-14 12:58:39 +0330

    Refactor

--- src/background.js
+++ src/background.js
@@ -4,20 +4,21 @@ let whiteList = []
 async function updateLists() {
     let blackRes = await fetch('https://raw.githubusercontent.com/sir-kokabi/no-delete/main/black-list.txt')
     let whiteRes = await fetch('https://raw.githubusercontent.com/sir-kokabi/no-delete/main/white-list.txt')
-    if (blackList.ok && whiteList.ok) {        
+    if (blackRes.ok && whiteRes.ok) {
         chrome.storage.local.set({ 'blackList': await blackRes.text() })
         chrome.storage.local.set({ 'whiteList': await whiteRes.text() })
         chrome.storage.local.set({ 'lastUpdated': (new Date() * 1) })
-    }    
+    }
 }
 
 async function runUpdater() {
     let black = (await chrome.storage.local.get('blackList')).blackList
     let white = (await chrome.storage.local.get('whiteList')).whiteList
+    console.log(white);
     if (typeof black != 'undefined' && typeof white != 'undefined') {
         blackList = black.split('\n')
         whiteList = white.split('\n')
-        
+
         let lastUpdated = (await chrome.storage.local.get('lastUpdated')).lastUpdated
 
         if (typeof lastUpdated == 'undefined' || lastUpdated < new Date().setDate(new Date().getDate() - 1)) {
@@ -30,30 +31,28 @@ async function runUpdater() {
 }
 
 (async () => {
-    await runUpdater()
-}
+    await runUpdater()}
 )()
 
 
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status == 'loading') {
+        //TODO: Extract the main domain and compare with it; for example: https://www.panel.domain.co.uk.com => domain.co.uk.com
+        if (blackList.find(u => tab.url.includes(u))) {
+            setBadge(tabId, 'You can NOT (easily) delete your account on this website! 😠', 'NO', '#FE0000')
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab)=>{    
-    if (changeInfo.status == 'loading'){
-        //let domain = getHostname(tab.url)
-        
-        if (blackList.find(u => tab.url.includes(u))){
-            chrome.action.setTitle({tabId:tabId, title:"You can NOT (easily) delete your account on this website! 😠"})
-            chrome.action.setBadgeText({tabId: tabId, text:"NO"})
-            chrome.action.setBadgeBackgroundColor({tabId: tabId, color: '#FE0000'})           
-        
-        } else if(whiteList.find(u => tab.url.includes(u))){
-            chrome.action.setTitle({tabId:tabId, title:"You CAN easily delete your account on this website 😍"})
-            chrome.action.setBadgeText({tabId: tabId, text:"YES"})
-            chrome.action.setBadgeBackgroundColor({tabId: tabId, color: '#00FF00'})   
-        }else{
-            chrome.action.setTitle({tabId:tabId, title:"Click and report if you know!"})
-            chrome.action.setBadgeText({tabId: tabId, text:"?"})
-            chrome.action.setBadgeBackgroundColor({tabId: tabId, color: '#000000'})  
+        } else if (whiteList.find(u => tab.url.includes(u))) {
+            setBadge(tabId, 'You CAN easily delete your account on this website 😍', 'YES', '#00FE00')
+
+        } else {
+            setBadge(tabId, 'Click and report if you know 🙏', '???', '#000000')           
         }
 
     }
 })
+
+function setBadge(tabId, title, text, backgroundColor) {
+    chrome.action.setTitle({ tabId: tabId, title: title })
+    chrome.action.setBadgeText({ tabId: tabId, text: text })
+    chrome.action.setBadgeBackgroundColor({ tabId: tabId, color: backgroundColor })
+}
